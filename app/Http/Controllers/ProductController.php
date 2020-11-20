32c3487696230da9b4075d70d5688846213e35e0
thinkverse <hallberg.kim@gmail.com>
2020-11-20 03:08:30 +0100

    Update ProductController Remove unused index method Add new show method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function show(Request $request, string $product)
     {
-        return view('welcome');
+        //
     }
 }
