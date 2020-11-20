5bc4044c01dfd83b469d68c83cf5d4500f7f7f3e
thinkverse <hallberg.kim@gmail.com>
2020-11-20 03:09:25 +0100

    Update routes/web.php Add new /products/{product} route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', WelcomeController::class);
+
+Route::get('/products/{product}', [ProductController::class, 'show']);
