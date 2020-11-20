200ef2b87e601a995f75a263f9d0dbd50d0f8391
thinkverse <hallberg.kim@gmail.com>
2020-11-20 02:41:26 +0100

    Update WelcomeController Return default welcome view from __invoke method

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,14 +6,8 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        //
+        return view('welcome');
     }
 }
